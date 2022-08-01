3ce0c96cca236bc6f207925ddebafa715cfd7fd2
José Júnior <59178745+josec-junior@users.noreply.github.com>
2022-08-01 11:19:42 -0300

    Update fatorial.py

--- EstruturaDeDados_2022.1/Recursividade/fatorial.py
+++ EstruturaDeDados_2022.1/Recursividade/fatorial.py
@@ -1,1 +1,10 @@
+def fatorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * fatorial(n - 1)
 
+
+n = int(input("Digite um número: "))
+fat = fatorial(n)
+print("O fatorial de", n, "é", fat)
