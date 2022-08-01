d4e2477b18eb49ced3e92b65ee999d0bc4ac9297
José Júnior <59178745+josec-junior@users.noreply.github.com>
2022-08-01 11:20:02 -0300

    Update fibonacci.py

--- EstruturaDeDados_2022.1/Recursividade/fibonacci.py
+++ EstruturaDeDados_2022.1/Recursividade/fibonacci.py
@@ -1,1 +1,10 @@
+def fibonacci(pos):
+  if pos == 1 or pos == 2:
+    return 1
+  else:
+    return fibonacci(pos - 1) + fibonacci(pos - 2)
 
+
+posicao = int(input("Digite a posição na sequência de Fibonacci que você deseja visualizar o número que está lá: "))
+num = fibonacci(posicao)
+print("O número que está nessa posição é o", num)
