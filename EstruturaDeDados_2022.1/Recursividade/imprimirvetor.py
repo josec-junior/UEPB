ecc0bed6e43a26acebb400faec663d42d4de7978
José Júnior <59178745+josec-junior@users.noreply.github.com>
2022-08-01 11:23:23 -0300

    Update imprimirvetor.py

--- EstruturaDeDados_2022.1/Recursividade/imprimirvetor.py
+++ EstruturaDeDados_2022.1/Recursividade/imprimirvetor.py
@@ -1,1 +1,10 @@
+def imprimir(vet, pos, tam):
+  if pos < tam:
+    print(vet[pos], end = " ")
+    imprimir(vet, pos + 1, tam)
 
+tamanho = int(input("Digite o tamanho do vetor: "))
+vetor = []
+for i in range(tamanho):
+  vetor.append(int(input("Digite um número: ")))
+imprimir(vetor, 0, tamanho)
