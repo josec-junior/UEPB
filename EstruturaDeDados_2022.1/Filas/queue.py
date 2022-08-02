d5352dcf2bc5214801f7f8fb725b7418b649af5f
José Júnior <59178745+josec-junior@users.noreply.github.com>
2022-08-02 17:38:37 -0300

    Update queue.py

--- EstruturaDeDados_2022.1/Filas/queue.py
+++ EstruturaDeDados_2022.1/Filas/queue.py
@@ -1,1 +1,29 @@
+class Queue:
 
+  def __init__(self):
+    self.head = []
+
+  def push(self, value):
+    self.head.append(value)
+
+  def pop(self):
+    return self.head.pop(0)
+
+  def __str__(self):
+    output = ""
+    if (self.isEmpty()):
+      output = "[]"
+    else:
+      for i in range(len(self.head)):
+        output += "[" + str(self.head[i]) + "] → "
+    return output
+
+  def isEmpty(self):
+    return len(self.head) == 0
+
+fila = Queue()
+fila.push("João")
+fila.push("Maria")
+fila.push("Álvaro")
+fila.push("Ana")
+print(fila)
